92ad35293b0b86802a84965f5c176b71759d9f22
Farida Dovletbayeva <farida02.03@icloud.com>
2025-04-29 23:26:27 +0500

    MyTestingClass class

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -1,4 +1,25 @@
 public class MyTestingClass {
+    private int id;
 
+    public MyTestingClass(int id) {
+        this.id = id;
+    }
 
+    @Override
+    public int hashCode() {
+        return (id * 31 + 17) % 9973;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass that = (MyTestingClass) obj;
+        return this.id == that.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Key#" + id;
+    }
 }
